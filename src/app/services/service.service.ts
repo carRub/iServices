66ae3c41cbab17682b083519e0e3ca54757dec2a
carRub <carub96@gmail.com>
2020-05-17 16:28:23 -0500

    fixed integration issues

--- src/app/services/service.service.ts
+++ src/app/services/service.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
 
-  constructor() { }
+  filtrados = [];
+  filtradosSubject = new Subject<[]>();
+
+  constructor(private http: HttpClient) { }
+
+  /*getServices():Promise<any>{
+    return this.http.get('http://localhost:3000/profesionistas').toPromise();
+  }*/
+
+  getServices(){
+    return this.http.get<any>('http://localhost:3000/profesionistas');
+
+  }
+
+  filtrandoProfesionistas(inputValue: string){
+
+  }
+
 }
